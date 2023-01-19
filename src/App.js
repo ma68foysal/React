e094c36b5f91bf7f729443e3cc8c4f93374742c3
ma68foysal <ahmedzzf770@gmail.com>
2023-01-19 21:01:55 +0600

    [Done]

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import react  from 'react';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+
 import './App.css';
-import StyleProvider from './context/contextProvider/StyleProvider';
-import ThemeProvider from './context/contextProvider/ThemeProvider';
-import UserProvider from './context/contextProvider/UserProvider';
-import About from './Pages/About';
-import Blog from './Pages/Blog';
-import Contact from './Pages/Contact';
-import Home from './Pages/Home';
+
 
 
 
@@ -15,23 +9,7 @@ function App() {
   
   return (
   <div>
-     <ThemeProvider>
-     <StyleProvider>
-      <UserProvider>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/contact' element={<Contact/>}/>
-            <Route path='/blog' element={<Blog/>}/>
-          </Routes>
-        </Router>
-        
-           
-
-    </UserProvider>
-    </StyleProvider>
-    </ThemeProvider>
+     
  
   </div>
 
--- src/context/Context.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react'
-
-
-export const users = createContext()
-export const styles  = createContext()
-export const themeSwitch = createContext()
