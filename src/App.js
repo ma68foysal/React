478af974cc04c863b332cc3c6fae7b512205c86e
ma68foysal <ahmedzzf770@gmail.com>
2023-01-19 18:04:39 +0600

    [Done]

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@ import './App.css';
 import StyleProvider from './context/contextProvider/StyleProvider';
 import ThemeProvider from './context/contextProvider/ThemeProvider';
 import UserProvider from './context/contextProvider/UserProvider';
-import Hero from './Hero/Hero';
-import Layout from './Layout/Layout';
-import AddPost from './Layout/Posts/AddPost';
-import Posts from './Layout/Posts/Posts';
 import About from './Pages/About';
 import Blog from './Pages/Blog';
 import Contact from './Pages/Contact';
 import Home from './Pages/Home';
-import Login from './Users/Login/Login';
-import Regi from './Users/Regi/Regi';
+
 
 
 function App() {
