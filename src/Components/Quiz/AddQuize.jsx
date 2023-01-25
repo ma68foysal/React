3463066677112a3f9f2f9c4de8c6a3c118a8ab20
ma68foysal <ahmedzzf770@gmail.com>
2023-01-25 21:30:19 +0600

    [DONE]

--- src/Components/Quiz/AddQuize.jsx
+++ src/Components/Quiz/AddQuize.jsx
@@ -1,29 +1,105 @@
 import React from 'react'
+
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useReducer } from 'react'
 import { Link } from 'react-router-dom'
+import { addQuiz } from '../../api'
 import ModalView from '../../Customs/ModalView'
 
 
+  
+
+
 function AddQuize() {
+
+  
+  
+  
+
+
+  const [img,setImage]=useState('./logo192.png')
+       
+  const initState ={
+    name: "",
+    description: "",
+    price: "",
+    duration: "",
+    answerType: "",
+    image: img,
+  }
+  const addQuize='addQuize'
+  function quizReducer(state,action){
+    switch(action.type){
+      case addQuize:
+        return {
+          ...state,
+          [action.payload.name]:action.payload.value
+        }
+        default:
+          return state
+    }
+  }
+  const [newQuize,dispatch]=useReducer(quizReducer,initState)
+       const {name,description, price,duration,answerType,image}=newQuize
+  console.log(newQuize);
+        
+  function uploadimg (e){
+    const reader = new FileReader()
+    reader.onload= ()=>{
+      if(reader.readyState===2){
+        setImage(reader.result)
+        
+      }
+    }
+    reader.readAsDataURL(e.target.files[0])
+  }
+
+  const oninputChange=(e)=>{
+    dispatch({
+      type:addQuize,
+      payload:{
+        name:e.target.name,
+        value:e.target.value
+        
+      }
+    }
+    )
+    
+  }
+ function handleOnSubmit(e){
+  e.preventdefault()
+ }
+  function hanleSubmit (){
+    
+    addQuiz(newQuize).then((data)=>
+    {
+      sessionStorage.getItem('quiz',JSON.stringify(data))
+    }
+    )
+  }
+  
   return (
     <>
     <ModalView>
-        <form 
-        // onSubmit={handleOnSubmit}
+        < 
+        
         >
           <div className="flex items-center my-4 ">
             <p className="text-center font-bold mx-4 mb-0 ">
             Upload an interesting quiz for your visitors
             </p>
           </div>
-          {/* <!-- Email input --> */}
+          {/* <!-- Name input --> */}
           <div className="mb-6">
             <label className='form-label'>
             A Short Name!
             </label>
             <input
               type="text"
-            
-              
+              name="name"
+              value={name}
+              onChange={oninputChange}
               required
               
             //   onChange={onloginState}
@@ -32,15 +108,15 @@ function AddQuize() {
             />
           </div>
 
-          {/* <!-- Password input --> */}
+          {/* <!-- Description input --> */}
           <div className="mb-6">
-          
           <label className='form-label'>A Short Description</label>
             <textarea
               type="text"
+              name="description"
+              value={description}
+              onChange={oninputChange}
               required
-              //   value={password}
-            //   onChange={onloginState}
               className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               placeholder="What would be quiz's best description, describe a short description and key point"
             />
@@ -52,7 +128,9 @@ function AddQuize() {
             </label>
             <input
               type="text"
-            
+              name="price"
+              value={price}
+              onChange={oninputChange}
               required
               
             //   onChange={onloginState}
@@ -60,6 +138,7 @@ function AddQuize() {
               placeholder="What is the type or name of quiz"
             />
           </div>
+          {/* <!-- time duration input --> */}
           <div className='mb-6'>
           <label>
           What would be the Quiz time duration?
@@ -69,9 +148,9 @@ function AddQuize() {
                 as="select"
                 id="answerType"
                 type="text"
-                name="answerType"
-                // value={answerType}
-                // onChange={onInputChange}
+                name="duration"
+                value={duration}
+                onChange={oninputChange}
                 required
               >
                 <option value="">Choose Quiz Duration</option>
@@ -89,8 +168,8 @@ function AddQuize() {
                 id="answerType"
                 type="text"
                 name="answerType"
-                // value={answerType}
-                // onChange={onInputChange}
+                value={answerType}
+                onChange={oninputChange}
                 required
               >
                 <option value="">Choose Quiz Answer Option</option>
@@ -104,9 +183,11 @@ function AddQuize() {
             </label>
             <input
               type="file"
-              accept='image/*'
-            
-              required
+              name='image'
+              accept="image/*"
+              // value={image}
+              onChange={uploadimg}
+              
               
             //   onChange={onloginState}
               className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
@@ -116,20 +197,12 @@ function AddQuize() {
               
 
           <div className="text-center lg:text-left ">
-            <button  className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
-              Login
+            <button onClick={hanleSubmit}  className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+              submit
             </button>
-            <p className="text-sm font-semibold mt-2 pt-1 mb-0">
-              Don't have an account?{" "}
-              <Link
-                to=""
-                className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
-              >
-                Register
-              </Link>
-            </p>
+            
           </div>
-        </form>
+        </>
       </ModalView>
     </>
   )
--- src/api.js
+++ src/api.js
@@ -24,3 +24,4 @@ import axios from "axios";
   export const registerUser = (userInfo) => Api.post('/api/user/register', userInfo)
   export const getQuizzes = () => Api.get('/api/quiz/all')
   export const get_Quiz_by_id = (id) => Api.get(`/api/quiz/${id}`)
+  export const addQuiz = (quizData) => Api.post(`/api/quiz/add`, quizData);
