317bd769f11c230ec236fa1a53b865f30867e961
ma68foysal <ahmedzzf770@gmail.com>
2023-01-28 12:43:01 +0600

    [DONE]

--- src/api.js
+++ src/api.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 
   const Api = axios.create({
-     baseURL: "https://backend-minhajsadik.vercel.app"
-    }
-  )
-  Api.interceptors.request.use((request,)=>{
-    console.log({request});
-    if(sessionStorage.getItem('token')){
-        request.headers.Authorization = `Bearer ${JSON.parse(sessionStorage.getItem('token')).token}`;
+     baseURL: "https://backend-minhajsadik.vercel.app",
+     withCredentials: true,
 
     }
-    return request
-  })
+  )
+
+  // Api.interceptors.request.use((request,)=>{
+  //   console.log({request});
+  //   if(sessionStorage.getItem('token')){
+  //       request.headers.Authorization = `Bearer ${JSON.parse(sessionStorage.getItem('token')).token}`;
+
+  //   }
+  //   return request
+  // })
 
   export const loginUser = (userInfo) => Api.post('/api/user/login', userInfo)
   export const registerUser = (userInfo) => Api.post('/api/user/register', userInfo)
@@ -20,20 +23,3 @@ import axios from "axios";
   export const addQuiz = (quizData) => Api.post(`/api/quiz/add`, quizData);
 
 
-//   import axios from "axios";
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly',
-//   params: {lat: '35.5', lon: '-78.5'},
-//   headers: {
-//     'X-RapidAPI-Key': '19c3693bb4msh1d4d19a7554c05ap13315fjsn96de8fe370c3',
-//     'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
-//   }
-// };
-
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
