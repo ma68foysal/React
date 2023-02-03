3af25785d12d8b59261bc39480866c1f3d2dac7f
ma68foysal <ahmedzzf770@gmail.com>
2023-02-03 19:03:10 +0600

    [DONE]

--- src/Components/SingleProductPage.jsx
+++ src/Components/SingleProductPage.jsx
@@ -1,18 +1,40 @@
 import React, { useContext } from 'react'
 import { CartItem } from '../context/context'
-
+import '../Style/style.css'
 function SingleProductPage() {
   const cartItems = useContext(CartItem)
   const {productId,setProductId} = cartItems
+  console.log(productId);
   
   
   return (
     <div>
     
     {
-       productId ? <div className="col-lg-4">
-       <div className="card" key={productId.id}>
-        <img src={productId.thumbnail} className="card-img-top" alt="..."/>
+       productId ? 
+       <div className="container">
+        <div className="row">
+
+        <div className="col-lg-6">
+          <div className='d-flex'>
+          <div>
+           {productId.images.map((productIMG)=>
+           <div>
+          <img className='product-img' src={productIMG} alt="" />
+           </div>
+              
+           )}
+          </div>
+       <div className="ms-2" key={productId.id}>
+        <img src={productId.thumbnail} className="card-img-top " alt="..."/>
+         </div>
+         {/* ./card */}
+          </div>
+          
+        </div>
+        {/* ./col-lg-4 left */}
+        <div className="col-lg-4">
+       <div className="" key={productId.id}>
         <div className="card-body">
          <h5 className="card-title">{productId.title}</h5>
          <p className="card-text">{productId.description}</p>
@@ -20,13 +42,16 @@ function SingleProductPage() {
          <p className='price'>{`$${productId.price}`}</p>
          <p className='price'>{productId.rating}</p>
          </div>
-         
-         
-         
-       </div>
          </div>
- 
- </div>
+         {/* ./card-body */}
+         </div>
+         {/* ./card */}
+        </div>
+        {/* ./col-lg-4 right */}
+        </div>
+        {/* row */}
+       </div>
+      
  :<h1>go to hell</h1>
     }
     
